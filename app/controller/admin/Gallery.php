490bd557dc5c3f4e1eded50a30cb7389c52b312c
Tiberiu Iulian Dafinescu <lordiulian@gmail.com>
2021-02-02 06:58:52 +0100

    modificar, agregar y eliminar datos BD

--- app/controller/admin/Gallery.php
+++ app/controller/admin/Gallery.php
@@ -12,6 +12,7 @@ use App\Controller\Http\Request;
 use App\Model\WebSite;
 use App\Model\Session;
 use App\Model\Theme;
+use App\Model\DataBase;
 
 class Gallery {
     
@@ -30,6 +31,70 @@ class Gallery {
             )
         );
         
+    }
+
+    public function link(): void {
+
+        Response::ok();
+
+        $request = new Request();
+        if(!$request->issetPOST(['file', 'title', 'description'])) {
+            Response::showJson([
+                'status' => 'error',
+                'message' => 'Debes completar todos los campos obligatorios.'
+            ]);
+            return;
+        }
+        $data = $request->getPOST();
+
+        $newImage = [];
+        $newImage['id'] = uniqid();
+        $newImage['title'] = $data['title'];
+        $newImage['description'] = $data['description'];
+        $newImage['file'] = $data['file'];
+
+        $allGallery = DataBase::load('gallery');
+        $allGallery[] = $newImage;
+
+        DataBase::create('gallery', $allGallery);
+
+        Response::showJson([
+            'status' => 'success',
+            'message' => 'El artículo se ha eliminado con éxito.'
+        ]);
+
+    }
+
+    public function unLink(): void {
+
+        Response::ok();
+
+        $request = new Request();
+        if(!$request->issetPOST(['id'])) {
+            Response::showJson([
+                'status' => 'error',
+                'message' => 'Debes completar todos los campos obligatorios.'
+            ]);
+            return;
+        }
+        $data = $request->getPOST();
+
+        $allGallery = DataBase::load('gallery');
+
+        foreach($allGallery as &$image) {
+            if ($image['id'] == $data['id']) {
+                unset($image);
+                break;
+            }
+        }
+
+        DataBase::create('news', $allGallery);
+
+        Response::showJson([
+            'status' => 'success',
+            'message' => 'La imagen se ha quitado de la galería con éxito.'
+        ]);
+
     }
     
 }
--- app/controller/admin/Navigation.php
+++ app/controller/admin/Navigation.php
@@ -12,6 +12,7 @@ use App\Controller\Http\Request;
 use App\Model\WebSite;
 use App\Model\Session;
 use App\Model\Theme;
+use App\Model\DataBase;
 
 class Navigation {
     
@@ -30,6 +31,69 @@ class Navigation {
             )
         );
         
+    }
+
+    public function add(): void {
+
+        Response::ok();
+
+        $request = new Request();
+        if(!$request->issetPOST(['title', 'url'])) {
+            Response::showJson([
+                'status' => 'error',
+                'message' => 'Debes completar todos los campos obligatorios.'
+            ]);
+            return;
+        }
+        $data = $request->getPOST();
+
+        $newLink = [];
+        $newLink['id'] = uniqid();
+        $newLink['title'] = $data['title'];
+        $newLink['url'] = $data['url'];
+
+        $allLinks = DataBase::load('links');
+        $allLinks[] = $newLink;
+
+        DataBase::create('links', $allLinks);
+
+        Response::showJson([
+            'status' => 'success',
+            'message' => 'El artículo se ha eliminado con éxito.'
+        ]);
+
+    }
+
+    public function unLink(): void {
+
+        Response::ok();
+
+        $request = new Request();
+        if(!$request->issetPOST(['id'])) {
+            Response::showJson([
+                'status' => 'error',
+                'message' => 'Debes completar todos los campos obligatorios.'
+            ]);
+            return;
+        }
+        $data = $request->getPOST();
+
+        $allLinks = DataBase::load('links');
+
+        foreach($allLinks as &$link) {
+            if ($link['id'] == $data['id']) {
+                unset($link);
+                break;
+            }
+        }
+
+        DataBase::create('links', $allLinks);
+
+        Response::showJson([
+            'status' => 'success',
+            'message' => 'El enlace se ha eliminado con éxito.'
+        ]);
+
     }
     
 }
