3746313933243ad580669d2eb2540bd28d2bff94
Tiberiu Iulian Dafinescu <lordiulian@gmail.com>
2021-03-10 12:26:34 +0100

    submit with ajax

--- themes/administrator/assets/js/main.js
+++ themes/administrator/assets/js/main.js
@@ -1,50 +1,124 @@
 
-
-
 window.onload = () => {
 
     // prepara todos los formularios
-    let form = document.getElementsByTagName("form");
-        for (let i = 0; i < form.length; i++) {
-            form[i].onsubmit = (e) => {
-                e.preventDefault();
-                if (form[i].ajax_url) {
-                    let data = new FormData(form[i]);
-                        data.delete('ajax_url');
-                        sendAjax(data, form[i].ajax_url);
-                } else {
-                    form[i].submit();
+    var form = document.getElementsByTagName("form");
+    for (var i = 0; i < form.length; i++) {
+        form[i].onsubmit = function(e) {
+
+            e.preventDefault();
+            var currentForm = e.currentTarget;
+
+            if (currentForm.ajax_url) {
+
+                var callback = null;
+                var data = new FormData(currentForm);
+                    data.delete('ajax_url');
+
+                if (currentForm.callback) {
+                    data.delete('callback');
+
+                    var validationStr = currentForm.callback.value;
+                    var cbFunction = window[validationStr];
+                    if (typeof cbFunction === "function") {
+                        callback = cbFunction;
+                    }
+
                 }
-            };
-        }
+
+                sendAjax(data, currentForm.ajax_url.value, callback);
+
+            } else {
+                currentForm.submit();
+            }
+            
+        };
+    }
 
 };
 
 
 
+function sendAjax(data, url, callback = null) {
 
-
-
-
-
-function sendAjax(data, url) {
-    
-	let xhttp = new XMLHttpRequest();
+	var xhttp = getAjaxObject();
 	xhttp.onreadystatechange = function() {
 
-		if(this.readyState == 4 && this.status == 200){
-			
-            let response = JSON.parse(this.responseText);
-            alert(response);
+		if(this.readyState == 4 && this.status == 200) {
+            try {
 
-		}else{
+                var response = JSON.parse(this.responseText);
+                if(response.status == 'error') {
+                    showAlert("alert-warning", response.message);
+                } else {
 
-            alert("error ajax:" + url);
+                    showAlert("alert-success", response.message);
+                    if (callback != null) {
+                        callback();
+                    }
 
+                }
+
+            } catch(e) {
+                showAlert("alert-danger", e);
+            }
 		}
 		
 	};
 	xhttp.open("POST", url, true);
 	xhttp.send(data);
 
+}
+function getAjaxObject(){
+    if (window.XMLHttpRequest) {
+        // code for modern browsers
+        return new XMLHttpRequest();
+    } else {
+        // code for old IE browsers
+        return new ActiveXObject("Microsoft.XMLHTTP");
+    }
+}
+
+
+
+
+/****************************
+* MOSTRAR ALERTAS DINÁMICAS *
+*****************************/
+function showAlert(css, msg, close = true){
+    var container = document.getElementById("alert-container");
+    var alert = document.createElement("div");
+        alert.classList.add("alert", 'fade', 'show', css);
+        alert.appendChild(document.createTextNode(msg));
+
+    if(close){
+
+        var button = document.createElement("button");
+            button.classList.add("close");
+            button.type = "button";
+            button.setAttribute("data-dismiss", "alert");
+            button.setAttribute("aria-label", "Close");
+
+        var icon = document.createElement("i");
+            icon.classList.add("fas", "fa-times");
+        var span = document.createElement("span");
+            span.setAttribute("aria-hidden", "true");
+            span.appendChild(icon);
+
+        button.appendChild(span);
+        alert.classList.add("alert-dismissible");
+        alert.appendChild(button);
+
+    }
+    container.appendChild(alert);
+}
+
+
+/**********************
+* ACTUALIZA LA PÁGINA *
+***********************/
+function redirectLogin() {
+    setTimeout(() => {
+        window.location.reload();
+    }, 3000);
 }
