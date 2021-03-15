46cd0d8ba634c983369bb933ca76c817240fafd2
Tiberiu Iulian Dafinescu <lordiulian@gmail.com>
2021-03-15 18:38:07 +0100

    progreso pagina de inicio

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -12,10 +12,43 @@ use App\Controller\Http\Request;
 use App\Model\WebSite;
 use App\Model\Session;
 use App\Model\Theme;
+use App\Model\DataBase;
+use App\Model\UploadedFile;
+use App\Model\GalleryFile;
 
 class Home {
     
     public function show(): void {
+
+        $dbFileList = DataBase::load('upload');
+        $fileList = [];
+        foreach($dbFileList as $dbFile) {
+            $tmpObj = new UploadedFile($dbFile['path']);
+            $tmpObj->setDescription($dbFile['description']);
+            $tmpObj->setPath($dbFile['path']);
+            $tmpObj->setId($dbFile['id']);
+            $fileList[] = $tmpObj;
+        }
+
+        $dbGalleryList = DataBase::load('gallery');
+        $gallery = [];
+        foreach($dbGalleryList as $dbGallery) {
+
+            $fileFound = null;
+            foreach($fileList as &$file) {
+                if($file->getId() == $dbGallery['file']) {
+                    $fileFound = $file;
+                    break;
+                }
+            }
+
+            $tmpObj = new GalleryFile();
+            $tmpObj->setDescription($dbGallery['description']);
+            $tmpObj->setTitle($dbGallery['title']);
+            $tmpObj->setId($dbGallery['id']);
+            $tmpObj->setFile($fileFound);
+            $gallery[] = $tmpObj;
+        }
         
         Response::ok();
         Response::write(
@@ -24,7 +57,9 @@ class Home {
                 [
                     'website' => new WebSite(), 
                     'session' => new Session(),
-                    'currentPage' => 'home'
+                    'currentPage' => 'home',
+                    'articleList' => DataBase::load('news'),
+                    'gallery' => $gallery
                 ]
             )
         );
